1448d0f664bbef4a21abb3a7d83a96b1e2885d62
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2019-12-04 18:03:51 +0700

    #1 add fake messages

--- src/main/java/ru/helpcontrol/sanbox/sarafan/controller/MessageController.java
+++ src/main/java/ru/helpcontrol/sanbox/sarafan/controller/MessageController.java
@@ -1,5 +1,9 @@
 package ru.helpcontrol.sanbox.sarafan.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("messages")
 public class MessageController {
 
+    // TODO: delete demo-fake messages
+    public List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{
+            put("id", "1");
+            put("text", "First message");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "2");
+            put("text", "Seccond message");
+        }});
+        add(new HashMap<String, String>() {{
+            put("id", "3");
+            put("text", "Third message");
+        }});
+    }};
+
     @GetMapping
-    public String index() {
-        return "index!";
+    public List<Map<String, String>> list() {
+        return messages;
     }
 }
