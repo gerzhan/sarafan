73d298e23a851e36fb6c26d8e665daf464670082
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2019-12-04 17:43:00 +0700

    #1 add MessageController

---
+++ src/main/java/ru/helpcontrol/sanbox/sarafan/controller/MessageController.java
@@ -0,0 +1,15 @@
+package ru.helpcontrol.sanbox.sarafan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("messages")
+public class MessageController {
+
+    @GetMapping
+    public String index() {
+        return "index!";
+    }
+}
