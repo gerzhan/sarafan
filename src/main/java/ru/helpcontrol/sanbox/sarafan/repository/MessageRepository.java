5efdec3676c1e78ec49fdcdb03d524121b221dd7
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2019-12-06 09:19:48 +0700

    #7 :bug: fix in MessageRepository

--- src/main/java/ru/helpcontrol/sanbox/sarafan/repository/MessageRepository.java
+++ src/main/java/ru/helpcontrol/sanbox/sarafan/repository/MessageRepository.java
@@ -1,8 +1,8 @@
 package ru.helpcontrol.sanbox.sarafan.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.helpcontrol.sanbox.sarafan.domain.Message;
 
-public interface MessageRepository extends CrudRepository<Message, Long> {
+public interface MessageRepository extends JpaRepository<Message, Long> {
 
 }
